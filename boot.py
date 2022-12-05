1ed3abda7ab1c68027f3dc459b01688cb6527e77
BANDARU NARASIMHAGUPTA <115392495+Narasimhagupta2004@users.noreply.github.com>
2022-12-05 21:58:31 +0530

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,13 @@
+#comment out
+#ever letter starts with small letters or capital letters or underscore
+#example
+#Class=11
+#or
+#Class
+#or
+#_gupta
+#kjasdfoia_djfla_sajnfj    #snake case
+#assignment operatos
+rent=200
+rent=45
+print(rent)
